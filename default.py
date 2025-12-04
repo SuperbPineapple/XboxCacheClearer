a042a732ae9e2e5c617326aabb7bbdf246db821b
Superb Pineapple <221645145+SuperbPineapple@users.noreply.github.com>
2025-12-03 20:34:56 -0600

    added XYZ cache partition clearers

--- default.py
+++ default.py
@@ -1,20 +1,42 @@
 import os
 
-# define cachepaths, you can change these if your cache path is different
-cachePath = r"E:\CACHE"
+# define cachepaths, you can change these if your caches are different
+cachePathE = r"E:\CACHE"
+cachePathX = r"X:"
+cachePathY = r"Y:"
+cachePathZ = r"Z:"
+# Scary and untested!
 
-# for if you play beta halos (you can comment this out if you don't play it)
+
+# for if you play beta halos (you can comment this out if this folder doesn't appear)
 betaHaloCache = r"E:\CACHEGSRV"
 
-# remove cache files
-for fileName in os.listdir(cachePath):
-   fileToRemove = os.path.join(cachePath, fileName)
-   if os.path.isfile(fileToRemove):
-      os.remove(fileToRemove)
+# remove E cache files
+for eCache in os.listdir(cachePathE):
+   clearECache = os.path.join(cachePathE, eCache)
+   if os.path.isfile(clearECache):
+      os.remove(clearECache)
 
-# remove halo cache files (you can comment this out if you don't play it)
+# remove X cache files
+for filesInX in os.listdir(cachePathX):
+   clearXCache = os.path.join(cachePathX, filesInX)
+   if os.path.isfile(clearXCache):
+      os.remove(clearXCache)
+
+# remove Y cache files
+for filesInY in os.listdir(cachePathY):
+   clearYCache = os.path.join(cachePathY, filesInY)
+   if os.path.isfile(clearYCache):
+      os.remove(clearYCache)
+
+# remove Z cache files
+for filesInZ in os.listdir(cachePathZ):
+   clearZCache = os.path.join(cachePathZ, filesInZ)
+   if os.path.isfile(clearZCache):
+      os.remove(clearZCache)
+
+# remove halo cache files (you can comment this out if the folder doesn't appear)
 for fileNameHalo in os.listdir(betaHaloCache):
    fileToRemoveHalo = os.path.join(betaHaloCache, fileNameHalo)
    if os.path.isfile(fileToRemoveHalo):
       os.remove(fileToRemoveHalo)
-
