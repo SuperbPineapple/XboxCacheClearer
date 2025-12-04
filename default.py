fd675b0ec68191935710068f9ddaf375abb6a6d9
Superb Pineapple <221645145+SuperbPineapple@users.noreply.github.com>
2025-12-03 22:36:19 -0600

    XBMC and derivatives don't use XYZ.. i believe

--- default.py
+++ default.py
@@ -5,8 +5,8 @@ cachePathE = r"E:\CACHE"
 # cachePathX = r"X:"
 # cachePathY = r"Y:"
 # cachePathZ = r"Z:"
-# There's no XYZ parititions on my version of Xemu, so these are disabled.
-# TODO - readd them (for other version that don't hide XYZ for some reason)
+# There's no XYZ parititions on XBMC that I can see.
+# If you can somehow see them, you can uncomment the XYZ partition code.
 
 # for if you play beta halos (you can comment this out if this folder doesn't appear)
 betaHaloCache = r"E:\CACHEGSRV"
@@ -19,21 +19,21 @@ for eCache in os.listdir(cachePathE):
 
 # remove X cache files
 # for filesInX in os.listdir(cachePathX):
-   clearXCache = os.path.join(cachePathX, filesInX)
-   if os.path.isfile(clearXCache):
-      os.remove(clearXCache)
+#  clearXCache = os.path.join(cachePathX, filesInX)
+#  if os.path.isfile(clearXCache):
+#     os.remove(clearXCache)
 
 # remove Y cache files
 # for filesInY in os.listdir(cachePathY):
-   clearYCache = os.path.join(cachePathY, filesInY)
-   if os.path.isfile(clearYCache):
-      os.remove(clearYCache)
+#  clearYCache = os.path.join(cachePathY, filesInY)
+#  if os.path.isfile(clearYCache):
+#     os.remove(clearYCache)
 
 # remove Z cache files
 # for filesInZ in os.listdir(cachePathZ):
-   clearZCache = os.path.join(cachePathZ, filesInZ)
-   if os.path.isfile(clearZCache):
-      os.remove(clearZCache)
+#  clearZCache = os.path.join(cachePathZ, filesInZ)
+#  if os.path.isfile(clearZCache):
+#     os.remove(clearZCache)
 
 # remove halo cache files (you can comment this out if the folder doesn't appear)
 for fileNameHalo in os.listdir(betaHaloCache):
